b514e2fbd79f92c33dae169d23d755ae33e850c6
Furkan <furkan.atmaca@boun.edu.tr>
2023-01-03 02:40:58 +0300

    update
    basic derivation is updated. Added a working example for preparing tableaux for latex from derivations.

--- developing.R
+++ developing.R
@@ -6,6 +6,7 @@ library(data.tree)
 
 source("./machinery2.0/updated_gen_functions.R")
 source("./machinery2.0/updated_eval_functions.R")
+source("./machinery2.0/updated_draw_latex.R")
 
 df <- read.csv("basic_numeration.csv", na.strings = "NA") %>% 
   mutate(mc = ifelse(is.na(mc), "", mc))
@@ -17,3 +18,7 @@ my_tree <- Clone(my_list[[4]])
 
 my_tree %>% agreeMC() %>% print("it","ft","ac","n_dominator","is_copy","mt_ac")
 
+my_trees <- mergeMC("DP1","V",numeration = df)
+my_trees %>% print("it","ft","ac","n_dominator","is_copy","mt_ac")
+
+my_trees %>% linear_tree()
--- machinery2.0/updated_draw_latex.R
+++ machinery2.0/updated_draw_latex.R
@@ -1,19 +1,24 @@
 library(tidyverse)
 library(magrittr)
 
+feat_names <- c("case","foc","wh")
+
 # Recursive functions to write trees
 ## draw simple linear tree with only labels recursively
 linear_tree <- function(my_tree){
   if(isLeaf(my_tree)){
     # Leaf node
     written <- paste0("[",
-                      ifelse(my_tree$it == 0, "", 
-                             ifelse(my_tree$is_copy == 1, paste0(my_tree$it,"c"), my_tree$it)),
+                      case_when(my_tree$it == 0 ~ "",
+                                my_tree$is_copy == 1 ~ paste0(my_tree$it,"c"),
+                                T ~ my_tree$it),
                       "$_{",
-                      ifelse(my_tree$is_copy == 1, "", my_tree$ft),
-                      "}$", " ",
-                      "$_{a:",
-                      ifelse(my_tree$is_copy == 1, "", my_tree$ac),
+                      case_when(my_tree$is_copy == 1 ~ "",
+                                any(unlist(str_split(my_tree$ft, "-"))==1) ~ paste(feat_names[which(unlist(str_split(my_tree$ft, "-"))==1)], collapse = ","),
+                                T ~ ""),
+                      case_when(my_tree$is_copy == 1 ~ "",
+                                any(unlist(str_split(my_tree$ac, "-"))==1) ~ paste0("a:",paste(feat_names[which(unlist(str_split(my_tree$ac, "-"))==1)], collapse = ",")),
+                                T ~ ""),
                       "}$",
                       "]")
   } else {
@@ -25,11 +30,12 @@ linear_tree <- function(my_tree){
                              ifelse(my_tree$is_copy == 1, paste0(my_tree$it,"c"), my_tree$it)),
                       "$_{",
                       case_when(my_tree$is_copy == 1 ~ "",
-                                is.null(my_tree$mc) ~ "",
-                                is.na(my_tree$mc) ~ "",
-                                !is.null(my_tree$mc) ~ my_tree$mc,
+                                any(unlist(str_split(my_tree$ft, "-")) ==1) ~ paste(feat_names[which(unlist(str_split(my_tree$ft, "-"))==1)], collapse = ","),
                                 T ~ ""),
-                      "}$",left_str," ",right_str,"]")
+                      case_when(my_tree$is_copy == 1 ~ "",
+                                any(unlist(str_split(my_tree$ac, "-"))==1) ~ paste0("a:", paste(feat_names[which(unlist(str_split(my_tree$ac, "-"))==1)], collapse = ",")),
+                                T ~ ""),
+                      "}$ ",left_str," ",right_str,"]")
   }
   return(written)
 }
